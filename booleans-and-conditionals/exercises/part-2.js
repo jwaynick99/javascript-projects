1a4ab7a5b92e2171dd4e55953cd16f3b24862171
jwaynick99 <jwaynick99@gmail.com>
2024-06-17 21:01:06 -0500

    Fixed typo for day 2 studio

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -31,9 +31,5 @@ if (shuttleSpeed > 17500) {
 }
 
 // 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
-if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
-    console.log("all systems go");
- } else {
-    console.log("WARNING. Not ready");
- }
-console.log(/* "Yes" or "No" */);
+
+console.log("Yes");
