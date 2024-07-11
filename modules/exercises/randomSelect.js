536c12ecbbe383311285973f229a0b814e05b825
jwaynick99 <jwaynick99@gmail.com>
2024-07-11 12:20:03 -0500

    Chapter 13 Modules - Examples and Exercises

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,8 @@
 function randomFromArray(arr){
-  //Your code here to select a random element from the array passed to the function.
+  let randomSelection = Math.floor(Math.random() * arr.length)
+  return arr[randomSelection]
 }
 
 //TODO: Export the randomFromArray function.
+
+module.exports = randomFromArray;
--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,18 @@
 // Import the modules exported from practiceExports.js below:
+
+const practice = require('./practiceExports.js');
+
+console.log(typeof practice);
+console.log(practice.isPalindrome('that'));
+console.log(practice.isPalindrome('radar'));
+
+console.log(practice)
+
+let arr = ['Hello', 'World', 123, 987, 'LC101'];
+
+console.log(practice.isPalindrome('mom'));
+console.log(practice.evenOrOdd(9));
+
+for (i=0; i < 3; i++){
+    console.log(practice.randomArrayElement(arr));
+}
