3ca02f436b6c7e1980b9968f53d930fc5c38eb0c
jwaynick99 <jwaynick99@gmail.com>
2024-08-03 23:15:08 -0500

    Chapter 23 DOM and Events - Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,24 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  // Put your code for the exercises here.
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston, we have liftoff!";
+  });
+
+  missionAbort.addEventListener("mouseover", (event) => {
+    missionAbort.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "";
+  });
+  missionAbort.addEventListener("click", (event) => {
+    if (window.confirm("Are you sure you want to abort the mission?")) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+  });
 }
 
 window.addEventListener("load", init);
