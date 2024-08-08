e38ed50351f049dede950a1bb732fb3083804efc
jwaynick99 <jwaynick99@gmail.com>
2024-08-08 16:08:08 -0500

    Chapter 25 - examples and exercise

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,25 @@
 //Code Your Solution Below
+window.addEventListener("load", () => {
+  let testName = document.getElementById("testName");
+  let testDate = document.getElementById("testDate");
+  let rocketType = document.getElementById("rocketType");
+  let boosterCount = document.getElementById("boosterCount");
+  let windRating = document.getElementById("windRating");
+  let productionServers = document.getElementById("productionServers");
+  let button = document.getElementById("submit");
+  button.addEventListener("click", () => {
+    if (
+      testName.value === "" ||
+      testDate.value === "" ||
+      rocketType.value === "" ||
+      boosterCount.value === "" ||
+      windRating.value === ""
+    );
+    {
+      alert("All fields required");
+      event.preventDefault();
+    }
+  });
+
+  console.log(testName.value);
+});
